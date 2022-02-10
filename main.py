a8bca020e6b0f4a89e768953647eb12799b67efe
Jay Gupta <jay002@e.ntu.edu.sg>
2022-02-10 17:05:29 +0800

    remove item endpoint

--- main.py
+++ main.py
@@ -49,11 +49,6 @@ app.add_middleware(
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-@app.get("/items/")
-async def read_items(token: str = Depends(oauth2_scheme)):
-    return {"token": token}
-
-
 # Database Dependency
 def get_db():
     db = SessionLocal()
