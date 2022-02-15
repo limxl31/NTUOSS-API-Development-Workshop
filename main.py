2f8d88586142483ebc0e826610bfb2bd60a74424
Jay Gupta <jay002@e.ntu.edu.sg>
2022-02-15 18:57:41 +0800

    deploy code

--- main.py
+++ main.py
@@ -5,51 +5,19 @@ by Jay Gupta for NTU Open Source Society
 """
 
 # Imports
-from fastapi import FastAPI, Depends, status, HTTPException
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi import FastAPI, Depends, status
 from pydantic import BaseModel
 from typing import List, Optional
 from database import SessionLocal
 from schema import DBMember
 from sqlalchemy import desc, asc
 from sqlalchemy.orm import Session
-from fastapi.middleware.cors import CORSMiddleware
-
-fake_users_db = {
-    "johndoe": {
-        "username": "johndoe",
-        "full_name": "John Doe",
-        "email": "johndoe@example.com",
-        "hashed_password": "fakehashedsecret",
-        "disabled": False,
-    },
-    "alice": {
-        "username": "alice",
-        "full_name": "Alice Wonderson",
-        "email": "alice@example.com",
-        "hashed_password": "fakehashedsecret2",
-        "disabled": True,
-    },
-}
 
 # Create a FastAPI Instance
 app = FastAPI()
 
-origins = [
-    "*",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
 # Database Dependency
+# Create a session for a request. 
 def get_db():
     db = SessionLocal()
     try:
@@ -64,71 +32,9 @@ class Member(BaseModel):
     school: str
     graduation_year: int
     
+    # Allow ORM fetch
     class Config:
         orm_mode = True
-
-
-class User(BaseModel):
-    username: str
-    email: Optional[str] = None
-    full_name: Optional[str] = None
-    disabled: Optional[bool] = None
-
-
-def fake_hash_password(password: str):
-    return "fakehashed" + password
-
-class UserInDB(User):
-    hashed_password: str
-
-
-def get_user(db, username: str):
-    if username in db:
-        user_dict = db[username]
-        return UserInDB(**user_dict)
-
-
-def fake_decode_token(token):
-    # This doesn't provide any security at all
-    # Check the next version
-    user = get_user(fake_users_db, token)
-    return user
-
-
-async def get_current_user(token: str = Depends(oauth2_scheme)):
-    user = fake_decode_token(token)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid authentication credentials",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    return user
-
-
-async def get_current_active_user(current_user: User = Depends(get_current_user)):
-    if current_user.disabled:
-        raise HTTPException(status_code=400, detail="Inactive user")
-    return current_user
-
-
-@app.post("/token")
-async def login(form_data: OAuth2PasswordRequestForm = Depends()):
-    user_dict = fake_users_db.get(form_data.username)
-    if not user_dict:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
-    user = UserInDB(**user_dict)
-    hashed_password = fake_hash_password(form_data.password)
-    if not hashed_password == user.hashed_password:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
-
-    return {"access_token": user.username, "token_type": "bearer"}
-
-
-@app.get("/users/me")
-async def read_users_me(current_user: User = Depends(get_current_active_user)):
-    return current_user
-
 
 # Methods for interacting with the SQLite Database
 # ------------------------------------------------
@@ -152,19 +58,20 @@ def create_member(db: Session, member: Member):
     return db_member
 # ------------------------------------------------
 
+
 # API Routes
 # ------------------------------------------------
 @app.post('/members/', response_model=Member)
-async def create_members_view(member: Member, db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
+def create_members_view(member: Member, db: Session = Depends(get_db)):
     db_member = create_member(db, member)
     return db_member
 
 @app.get('/members/', response_model=List[Member])
-async def get_members_view(db: Session = Depends(get_db), sort_by: Optional[str] = None):
+def get_members_view(db: Session = Depends(get_db), sort_by: Optional[str] = None):
     return get_members(db, sort_by)
 
 @app.get('/member/{member_id}')
-async def get_member_view(member_id: int, db: Session = Depends(get_db)):
+def get_member_view(member_id: int, db: Session = Depends(get_db)):
     return get_member(db, member_id)
 # ------------------------------------------------
 
